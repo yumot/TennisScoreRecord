e330874bbdbe9afa78d014926ba28c9b85a7ffad
yumoto <yumo.tennis.20@outlook.jp>
2022-11-29 21:04:00 +0900

    empty activity

---
+++ app/src/main/java/com/yumo/tennis/score/tennis/score/tennis/score/record/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yumo.tennis.score.tennis.score.tennis.score.record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
