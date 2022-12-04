c90bc874b92e306470a84ac767151daeb1620a07
yumoto <yumo.tennis.20@outlook.jp>
2022-12-04 19:49:34 +0900

    スプラッシュ画面

--- app/src/main/java/com/yumo/tennis/score/record/MainActivity.kt
+++ app/src/main/java/com/yumo/tennis/score/record/MainActivity.kt
@@ -2,10 +2,13 @@ package com.yumo.tennis.score.record
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_TennisScoreRecord)
         setContentView(R.layout.activity_main)
     }
 }
